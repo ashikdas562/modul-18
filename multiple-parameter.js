e0ffd36d51871533d613df8c72f34f7abe03898a
unknown <ashikdas652.com>
2021-08-09 02:03:17 +0600

    git commit

---
+++ multiple-parameter.js
@@ -0,0 +1,35 @@
+function addTwoNumbers(number1, number2){
+     console.log(number1 , number2);
+    var total = number1 + number2;
+    return result;
+
+}
+var firstNumber = 35;
+var secondNumber = 47;
+var result = addTwoNumbers(firstNumber, secondNumber);
+console.log("result value", result) 
+
+// multiple 
+
+function multiplyTwoNumbers(num1, num2){
+    var result = num1 * num2;
+    return result;
+}
+var total = multiplyTwoNumbers(5, 100);
+console.log("total after multiplacation", total)
+
+// biog koro 2 ta
+function SubtractionTwoNumbers(n1, n2){
+    var result = n1 - n2;
+    return result;
+}
+var value  = SubtractionTwoNumbers(50,25);
+console.log("biog dilam", value)
+
+// division
+function divisionTwoNumbers(num1, num2){
+    var result = num1 / num2;
+    return result
+}
+var motmat = divisionTwoNumbers(100, 10);
+console.log("vag fol", motmat)
