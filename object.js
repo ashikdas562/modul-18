b0263c5651d5de6896e651e0eed99ebdf5534db6
unknown <ashikdas652.com>
2021-08-09 12:45:31 +0600

    commit

---
+++ object.js
@@ -0,0 +1,14 @@
+var student1 = {id: 115, name: "Abir Adnan", marks: 81, phone: 8794654 };
+var mobile = {color: "black", pprice: 19000, screanSize: 5, Storage: "16gb"};
+var book = {title: "start with why", author:"saimon sinek", price:200,pages:246, publisher: "penguin "}
+
+//  multiple way to get and set object property 
+var computer = {
+    price: 29000,
+    prossecor: "intel 5",
+    color: "silver",
+    Storage: "156gb",
+
+}
+computer.price = 20000;
+ console.log(computer.price);
