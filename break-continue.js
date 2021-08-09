ba2b3ed10bf70e45163fde5897e8001dff0ed452
unknown <ashikdas652.com>
2021-08-10 01:29:28 +0600

    commit

--- break-continue.js
+++ break-continue.js
@@ -17,10 +17,18 @@ for (var i = 0; i < 20; i++) {
 // Array
 
 var numbers = [54, 35, 21, 98, 23, 101, 45, 67]
-for (var i = 0; i<numbers.length; i++){
+// for (var i = 0; i<numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+//     if (number > 90) {
+//         break;
+//     }
+// }
+
+for (var i = 0; i < numbers.length; i++) {
     var number = numbers[i];
-    console.log(number);
     if (number > 90) {
-        break;
+        continue
     }
+    console.log(number);
 }
---
+++ summary.js
@@ -0,0 +1,22 @@
+// variable 
+var name = 'value';
+
+// array 
+
+var phone = ['iphone', 'xaomi', 'samsung', 'lg', 'htc'];
+// changing value 
+phone[2] = 'walton';
+
+// check oppo exists in an array 
+if(phone.indexOf('oppo') == -1 ){
+    console.log('opps amir khan oppo is missing');
+}
+// if lg is aviable 
+if (phone.indexOf('lg') != -1){
+    console.log("lg is aviable now ");
+}
+// loop 
+var num =0;
+while(num <= 10){
+    num++;
+}
